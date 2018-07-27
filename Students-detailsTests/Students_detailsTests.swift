7872bc1dc43670b9b2149079ccea1b91684b02c4
Anand <anand@anands-mac-pro.local>
2018-07-27 11:57:03 +0530

    Initial Commit

---
+++ Students-detailsTests/Students_detailsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Students_detailsTests.swift
+//  Students-detailsTests
+//
+//  Created by Anand on 27/07/18.
+//  Copyright © 2018 Rahul. All rights reserved.
+//
+
+import XCTest
+@testable import Students_details
+
+class Students_detailsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
